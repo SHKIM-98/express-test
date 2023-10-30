215056df0a69222ea906c8139ed42abf0570682b
SHKIM-98 <cubix4549@gmail.com>
2023-10-30 17:01:42 +0900

    test code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express') //express를 설치했기 때문에 가져올
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('SHKIM-TEST')
+  res.send('SHKIM-TEST : TEST CODE')
 })
 
 app.listen(3000,()=>	{
