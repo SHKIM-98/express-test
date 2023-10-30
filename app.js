4d9662883c8c8fb40dd4a63642da9b6db41d494e
SHKIM-98 <cubix4549@gmail.com>
2023-10-30 10:56:07 +0900

    first test commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express') //express를 설치했기 때문에 가져올 수 있다.
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('SHKIM-TEST')
+})
+
+app.listen(3000,()=>	{
+	console.log("LISTEN : 3000");
+})
